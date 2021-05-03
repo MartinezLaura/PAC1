1a89379c76e0c651b595f65174ef280886f7c25c
Laura Martinez Sanchez <lmartisa@gmail.com>
2021-05-03 21:05:13 +0200

    Update funciones.py

--- funciones.py
+++ funciones.py
@@ -38,7 +38,7 @@ def plot(img, name, space):
   for idx, col in enumerate(color_legend):
     histr = cv2.calcHist([img],[idx],None,[256],[0,256])
     ax.plot(histr, color = col, label = name[idx])
-    ax.xlim([0,256])
+    ax.set_xlim([0,256])
   leg = ax.legend(loc='best')
   for l in leg.legendHandles:
     l.set_linewidth(10)
