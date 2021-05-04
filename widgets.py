535f4499bda337132f00ce175087729f60ade77d
Laura Martinez Sanchez <lmartisa@gmail.com>
2021-05-04 14:01:00 +0200

    Update widgets.py

--- widgets.py
+++ widgets.py
@@ -120,19 +120,6 @@ def widget_RGB():
         readout_format='.2f',
     )
 
-    # cargamos todas las imagenes que tenemos
-    image_list = []
-    counter = 1
-    for file in os.listdir(path_img):
-      image_list.append((file,counter))
-      counter += 1
-
-    img_dropdown = widgets.Dropdown(
-        options=image_list,
-        value=2,
-        description='Test Image:',
-    )
-    
     return [intensity_slider, min_brightness_slider, min_ratio_slider, max_ratio_slider, radius_slider]
 
 def img_slid(path_img):
